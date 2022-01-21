d9e06470643474245f159137956f445569e5e399
andreeamiclaus <96205415+andreeamiclaus@users.noreply.github.com>
2022-01-21 17:46:03 +0100

    Added relevant icon from API (with the API icon URL)

--- src/app.js
+++ src/app.js
@@ -21,12 +21,19 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon"); //search for the icon URL link
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000); //To get the time and to convert it in milliseconds thats why it is *1000
+  //This setAttribute will change the icon with the one in the URL link.You can remove the image you had in src now
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description); //we do this for the image to look better near description and temperature
 }
 
 let apiKey = "b1c7074725f017b97210de1d82e98750";
